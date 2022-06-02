927ab6910b0d6b40d679be7662e07f4223b5aba8
Alan Pearce <alan.pearce@alttag.org>
2022-06-02 08:15:55 -0400

    Remove dead code

--- src/useHookedComponent.js
+++ src/useHookedComponent.js
@@ -1,25 +1,3 @@
-// import React, { useState } from 'react';
-
-// function useHookedComponent(Component, initial, setters) {
-//     const [returnState] = useState(() => {
-//         let state = initial;
-//         let setState = () => {};
-//         const returnSetters = !!setters ? 
-//             setters.map(setter => (...args) => setState(current => setter(...args, current))) :
-//             [(v) => setState(v)];
-
-//         return [function HookedComponent(props) {
-//             [state, setState] = useState(initial);
-
-//             return (<Component {...props} {...state} />);
-//         }, ...returnSetters, () => state];
-//     });
-
-//     return returnState;
-// };
-
-// export default useHookedComponent;
-
 import * as React from "react";
 import { useState } from "react";
 
